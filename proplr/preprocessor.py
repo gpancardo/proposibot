4828111f54b9c209b704784814a8e55dac201f7a
gpancardo <ghpancardo@gmail.com>
2025-08-13 17:29:21 -0600

    Formatting mistakes fixed

--- proplr/preprocessor.py
+++ proplr/preprocessor.py
@@ -1,32 +1,34 @@
 import unicodedata
 from nltk.corpus import stopwords
+import re
+
 
 class Preprocessor:
-    def init(self):
+    def __init__(self):
+        # Quantifiers
+        self.quantifiers = {
+            'todos': 'CUANT_TODO', 'todas': 'CUANT_TODO', 'todo': 'CUANT_TODO', 'toda': 'CUANT_TODO',
+            'ningún': 'CUANT_NINGUN', 'ninguna': 'CUANT_NINGUN', 'ningunos': 'CUANT_NINGUN', 'ningunas': 'CUANT_NINGUN',
+            'algunos': 'CUANT_ALGUNOS', 'algunas': 'CUANT_ALGUNOS', 'algun': 'CUANT_ALGUNOS', 'alguna': 'CUANT_ALGUNOS',
+            'ciertos': 'CUANT_ALGUNOS', 'ciertas': 'CUANT_ALGUNOS', 'cierto': 'CUANT_ALGUNOS', 'cierta': 'CUANT_ALGUNOS'
+        }
         
-        def __init__(self):
-            # Cuantifiers
-            self.quantifiers = {
-                'todos': 'CUANT_TODO', 'todas': 'CUANT_TODO', 
-                'ningún': 'CUANT_NINGUN', 'ninguna': 'CUANT_NINGUN',
-                'algunos': 'CUANT_ALGUNOS', 'algunas': 'CUANT_ALGUNOS',
-                'ciertos': 'CUANT_ALGUNOS', 'ciertas': 'CUANT_ALGUNOS'
-            }
-            
-            #Relations and negations
-            self.relation_verbs = {'es', 'son', 'fue', 'fueron','sera', 'seran', 'seria', 'serian','ha sido', 'han sido', 'habra sido'}
-            self.negation_terms = {'no', 'ni', 'nunca', 'jamas', 'sin'}
-            
-            self.stopwords_es = set(stopwords.words('spanish'))
-            self.stopwords_es -= {'no', 'ni'}
+        # Relation and negation
+        self.relation_verbs = {'es', 'son', 'fue', 'fueron', 'sera', 'seran', 'seria', 'serian', 'ha sido', 'han sido', 'habra sido'}
+        self.negation_terms = {'no', 'ni', 'nunca', 'jamas', 'sin'}
         
+        # Removes non-useful stopwords
+        self.stopwords_es = set(stopwords.words('spanish'))
+        self.stopwords_es -= self.negation_terms
+
+        # Removes special characters
+        self.no_special_chars = re.compile(r'[^\w\s]')
+    
+    def normalize_text(self, text):
+        text = text.lower()
+        text = ''.join(
+            c for c in unicodedata.normalize('NFD', text)
+            if unicodedata.category(c) != 'Mn'
+        )
+        return text
         
-        def normalize_text(text):
-            text = text.lower()
-            text = ''.join(
-                c for c in unicodedata.normalize('NFD', text)
-                #Remove markers for accents
-                if unicodedata.category(c) != 'Mn'
-            )
-            return text
-        self.normalize_text = normalize_text
